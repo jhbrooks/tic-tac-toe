d529e25c92730b2b164c2403c4aa42e787bf7957
James Brooks <jhbrooks@uchicago.edu>
2015-10-29 16:56:41 -0400

    refactor to reduce dependencies and improve readability

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,197 +1,195 @@
+# operates the Tic Tac Toe game
 class Game
-	def initialize(p1, p2, dimension = 3)
-		@p1 = p1
-		@p2 = p2
-		@board = Board.new(dimension)
-		@last_move = [nil, nil]
-		@line_width = 80
-	end
+  def initialize(p1, p2, board)
+    @p1 = p1
+    @p2 = p2
+    @board = board
+    @last_move_row = nil
+    @last_move_column = nil
+    @line_width = 80
+  end
 
-	def start
-		take_turn(p1)
-		return nil
-	end
+  def start
+    take_turn(p1)
+    nil
+  end
 
-	private
+  private
 
-	def take_turn(player)
-		puts
-		puts "It is #{player.name}'s turn.".center(line_width)
-		draw_board
-		move(player)
-		if win?
-			win(player)
-		elsif board.full?
-			tie
-		else
-			take_turn(other_player(player))
-		end
-	end
+  attr_reader :p1, :p2, :board, :line_width
+  attr_accessor :last_move_row, :last_move_column
 
-	def draw_board
-		puts
-		row_graphic_strings = board.squares.map do |row|
-			row.join(" | ").center(line_width)
-		end
-		dash_string = ("--#{'-' * board.dimension}#{'---' * (board.dimension - 1)}").center(line_width)
-		board_graphic_string = row_graphic_strings.join("\n#{dash_string}\n")
-		puts board_graphic_string
-	end
+  def take_turn(player)
+    puts
+    puts "It is #{player.name}'s turn.".center(line_width)
+    draw_board
+    move(player)
+    if win?
+      win(player)
+    elsif board.full?
+      tie
+    else
+      take_turn(other_player(player))
+    end
+  end
 
-	def move(player)
-		puts
-		puts "Starting from the top, choose the row where you'd like to play."
-		row = gets.chomp!.to_i
-		puts "Starting from the left, choose the column where you'd like to play."
-		column = gets.chomp!.to_i
-		if !(row.between?(1, board.dimension)) || !(column.between?(1, board.dimension))
-			puts "Invalid move! Please try again."
-			move(player)
-		elsif board.squares[row - 1][column - 1] != " "
-			puts "Space occupied! Please try again."
-			move(player)
-		else
-			update_board(row, column, player)
-			self.last_move = [(row - 1), (column - 1)]
-		end
-	end
+  def draw_board
+    puts
+    puts board.draw(line_width)
+  end
 
-	def win?
-		row_win? || column_win? || diagonal_win?
-	end
+  def move(player)
+    puts
+    puts "Starting from the top, choose the row where you'd like to play."
+    row = gets.chomp!.to_i
+    puts "Starting from the left, choose the column where you'd like to play."
+    column = gets.chomp!.to_i
+    if !(row.between?(1, board.dimension)) || !(column.between?(1, board.dimension))
+      puts "Invalid move! Please try again."
+      move(player)
+    elsif board.square(row, column) != " "
+      puts "Space occupied! Please try again."
+      move(player)
+    else
+      board.update(row, column, player_mark(player))
+      self.last_move_row = row
+      self.last_move_column = column
+    end
+  end
 
-	def row_win?
-		board.squares[last_move[0]].all? do |square|
-			square == board.squares[last_move[0]][last_move[1]]
-		end
-	end
+  def win?
+    row_win? || column_win? || diagonal_win?
+  end
 
-	def column_win?
-		squares_to_check = []
-		board.dimension.times do |i|
-			squares_to_check << (board.squares[i - 1][last_move[1]])
-		end
-		squares_to_check.all? do |square|
-			square == board.squares[last_move[0]][last_move[1]]
-		end
-	end
+  def row_win?
+    squares_to_check = []
+    board.dimension.times do |i|
+      squares_to_check << (board.square(last_move_row, i))
+    end
+    squares_to_check.all? do |square|
+      square == last_move_square
+    end
+  end
 
-	def diagonal_win?
-		down_diagonal_win? || up_diagonal_win?
-	end
+  def column_win?
+    squares_to_check = []
+    board.dimension.times do |i|
+      squares_to_check << (board.square(i, last_move_column))
+    end
+    squares_to_check.all? do |square|
+      square == last_move_square
+    end
+  end
 
-	def down_diagonal_win?
-		squares_to_check = []
-		1.upto(board.dimension) do |i|
-			squares_to_check << (board.squares[i - 1][i - 1])
-		end
-		squares_to_check.all? do |square|
-			square == board.squares[last_move[0]][last_move[1]]
-		end
-	end
+  def diagonal_win?
+    down_diagonal_win? || up_diagonal_win?
+  end
 
-	def up_diagonal_win?
-		squares_to_check = []
-		board.dimension.downto(1) do |i|
-			squares_to_check << (board.squares[i - 1][board.dimension - i])
-		end
-		squares_to_check.all? do |square|
-			square == board.squares[last_move[0]][last_move[1]]
-		end
-	end
+  def down_diagonal_win?
+    squares_to_check = []
+    1.upto(board.dimension) do |i|
+      squares_to_check << (board.square(i, i))
+    end
+    squares_to_check.all? do |square|
+      square == last_move_square
+    end
+  end
 
-	def win(player)
-		puts
-		puts "#{player.name} has won!".center(line_width)
-		draw_board
-		puts
-	end
+  def up_diagonal_win?
+    squares_to_check = []
+    board.dimension.downto(1) do |i|
+      squares_to_check << (board.square(i, (board.dimension - i + 1)))
+    end
+    squares_to_check.all? do |square|
+      square == last_move_square
+    end
+  end
 
-	def tie
-		puts
-		puts "It's a tie!".center(line_width)
-		draw_board
-		puts
-	end
+  def last_move_square
+    board.square(last_move_row, last_move_column)
+  end
 
-	def other_player(player)
-		if player == p1
-			return p2
-		else
-			return p1
-		end
-	end
+  def win(player)
+    puts
+    puts "*** #{player.name} has won! ***".center(line_width)
+    draw_board
+    puts
+  end
 
-	def update_board(row, column, player)
-		board.squares[row - 1][column - 1] =
-			if player == p1
-				"X"
-			else
-				"O"
-			end
-	end
+  def tie
+    puts
+    puts "*** It's a tie! ***".center(line_width)
+    draw_board
+    puts
+  end
 
-	def p1
-		@p1
-	end
+  def other_player(player)
+    if player == p1
+      return p2
+    else
+      return p1
+    end
+  end
 
-	def p2
-		@p2
-	end
-
-	def board
-		@board
-	end
-
-	def last_move
-		@last_move
-	end
-
-	def last_move=(row_column_pair)
-		@last_move = row_column_pair
-	end
-
-	def line_width
-		@line_width
-	end
+  def player_mark(player)
+    if player == p1
+      "X"
+    else
+      "O"
+    end
+  end
 end
 
+# stores board information; handles modification, retrieval, and display
 class Board
-	def initialize(dimension)
-		@dimension = dimension
-		@squares = []
-		dimension.times do
-			@squares << []
-			dimension.times do
-				@squares[-1] << " "
-			end
-		end
-	end
+  attr_reader :dimension
 
-	def full?
-		squares.all? do |row|
-			row.none? { |square| square == " " }
-		end
-	end
+  def initialize(dimension)
+    @dimension = dimension
+    @rows = []
+    dimension.times do
+      @rows << []
+      dimension.times do
+        @rows[-1] << " "
+      end
+    end
+  end
 
-	def dimension
-		@dimension
-	end
+  def full?
+    rows.all? do |row|
+      row.none? { |square| square == " " }
+    end
+  end
 
-	def squares
-		@squares
-	end	
+  def update(row, column, mark)
+    rows[row - 1][column - 1] = mark
+  end
+
+  def square(row, column)
+    rows[row - 1][column - 1]
+  end
+
+  def draw(line_width)
+    row_graphic_strings = rows.map do |row|
+      row.join(" | ").center(line_width)
+    end
+    dash_string = ("--#{'-' * dimension}#{'---' * (dimension - 1)}").center(line_width)
+    row_graphic_strings.join("\n#{dash_string}\n")
+  end
+
+  private
+
+  attr_reader :rows
 end
 
+# stores player information; handles retrieval
 class Player
-	def initialize(name)
-		@name = name
-	end
+  attr_reader :name
 
-	def name
-		@name
-	end
+  def initialize(name)
+    @name = name
+  end
 end
 
-g = Game.new(Player.new("Player 1"), Player.new("Player 2"))
+g = Game.new(Player.new("Player 1"), Player.new("Player 2"), Board.new(3))
 g.start
